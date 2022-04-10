bfff5feb24fc7a9b20c8a6c9fadffe9a46e5dc49
mirjamjago <90257253+mirjamjago@users.noreply.github.com>
2022-04-10 11:07:12 +0300

    list update

--- src/app/components/main-shop/main-shop.component.ts
+++ src/app/components/main-shop/main-shop.component.ts
@@ -8,13 +8,25 @@ import { Book } from 'src/app/models/book';
 })
 export class MainShopComponent implements OnInit {
   
-  booklist: Book[] | undefined;
+  booklist: any[] = [
+new Book(1, "Harry Potter", "J.K. Rowling", "Fantasy", "Book is in good contition", 10),
+new Book(1, "Harry Potter", "J.K. Rowling", "Fantasy", "Book is in good contition", 10),
+new Book(1, "Harry Potter", "J.K. Rowling", "Fantasy", "Book is in good contition", 10),
+new Book(1, "Harry Potter", "J.K. Rowling", "Fantasy", "Book is in good contition", 10)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  
+  /*
+  id?: number
+  bookname?: string;
+  author?: string;
+  genre?: string;
+  comment?: string;
+  price?: number;
+  */
 
 }
