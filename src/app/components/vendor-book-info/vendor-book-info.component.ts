267b363d77748481c2e6450bf4c78e462f7ca6ef
mirjamjago <90257253+mirjamjago@users.noreply.github.com>
2022-04-10 12:48:00 +0300

    change button

--- src/app/components/vendor-book-info/vendor-book-info.component.ts
+++ src/app/components/vendor-book-info/vendor-book-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book';
 
 @Component({
   selector: 'app-vendor-book-info',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendor-book-info.component.css']
 })
 export class VendorBookInfoComponent implements OnInit {
+  booklist: Book[] = [
+    new Book(1, "Harry Potter", "Jk.Rowling", "Fantacy", "Good contition", 10)
+  ];
+  currentEdit: Book | null = null;
 
   constructor() { }
 
